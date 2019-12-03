41e66e34539a02da62ee30b3b50277bd4abf9ed3
Daniel Benito <47041364+DBENITOD@users.noreply.github.com>
2019-12-02 21:36:26 -0500

    Cuarta carga

--- BembosPedidos/settings.py
+++ BembosPedidos/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR, '/static')
+#STATICFILES_DIRS = (BASE_DIR, '/static')
 
 REST_FRAMEWORK = {
     #'DEFAULT_PERMISSION_CLASSES': [
@@ -136,5 +136,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 5
 }
 
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
