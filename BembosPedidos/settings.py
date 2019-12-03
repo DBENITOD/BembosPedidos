72ff12a5774fec67e711f7e8cce9ce40d0713156
Daniel Benito <47041364+DBENITOD@users.noreply.github.com>
2019-12-02 21:50:10 -0500

    Carga de Reinicio 2

--- BembosPedidos/settings.py
+++ BembosPedidos/settings.py
@@ -129,7 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+#STATIC_URL = '/static/'
 #STATICFILES_DIRS = (BASE_DIR, '/static')
 
 REST_FRAMEWORK = {
@@ -142,5 +145,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 5
 }
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = 'media'
