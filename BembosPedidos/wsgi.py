453cd33037c6e9ecc1ee1a53087190d87c24b7c7
Daniel Benito <47041364+DBENITOD@users.noreply.github.com>
2019-12-04 14:27:06 -0500

    Reinicio 2

--- BembosPedidos/wsgi.py
+++ BembosPedidos/wsgi.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BembosPedidos.settings')
+
 env_path = Path('.')/ '.env'
 load_dotenv(dotenv_path=env_path)
 
