308f5c89af500677c71697866bede68686d26fcd
Daniel Benito <47041364+DBENITOD@users.noreply.github.com>
2019-12-04 14:32:55 -0500

    Reinicio 4

--- BembosPedidos/settings.py
+++ BembosPedidos/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 STATICFILES_DIRS = (BASE_DIR, '/static')
 
 REST_FRAMEWORK = {
@@ -146,5 +146,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 5
 }
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = 'media'
